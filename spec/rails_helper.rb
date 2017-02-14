5f0b9760f04c40f201a7384ddc0f188d20b0e07d
sdsd08013 <iwasaki.sukai@slogan.jp>
2017-02-15 00:24:16 +0900

    不要な記述の削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,9 +29,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
-  config.include Warden::Test::Helpers
   config.extend AuthenticationMacros
 
   # ファクトリを簡単に呼び出せるよう、Factory Girl の構文をインクルードする。
--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
-  describe 'with authentication' do
-    login
-    describe 'GET #index' do
-      it 'is valid with authenticated' do
+  describe 'GET #index' do
+    context 'with authentication' do
+      login
+      it 'return success status' do
         get '/pages'
         expect(response).to have_http_status(:success)
       end
     end
-  end
-  describe 'without authentication' do
-    describe 'GET #index' do
-      it 'is invalid without authenticated' do
+    context 'without authentication' do
+      it 'return fail status' do
         get '/pages'
         expect(response).to have_http_status(302)
       end
